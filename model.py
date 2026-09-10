a0242f49e63cc7072a1254bdf8452d3cc15934cf
DannyChee1 <officialjoebrabs@gmail.com>
2026-09-09 22:36:24 -0400

    feat: multiple batches

--- model.py
+++ model.py
@@ -70,7 +70,9 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
-    def forward(self, idx):
+        self.transformer.wte.weight = self.lm_head.weight
+
+    def forward(self, idx, targets=None):
         device = idx.device
         b, t = idx.size()
         assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
@@ -82,7 +84,10 @@ class GPT(nn.Module):
             x = block(x)
         x = self.transformer.ln_f(x)
         logits = self.lm_head(x)
-        return logits
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
 
     @classmethod
     def from_pretrained(cls, model_type):
--- train.py
+++ train.py
@@ -1,6 +1,32 @@
 import torch
 import tiktoken
 
+class DataLoader:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        with open("text.txt", "r") as f:
+            text = f.read()
+        enc = tiktoken.get_encoding("gpt2")
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens, dtype=torch.long)
+        print(f"Loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        self.current_pos = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_pos:self.current_pos + B * T + 1]
+        x = buf[:-1].view(B, T)
+        y = buf[1:].view(B, T)
+        self.current_pos += B * T
+
+        if self.current_pos + B * T + 1 > len(self.tokens):
+            self.current_pos = 0
+        return x, y
+
 from model import GPT, GPTConfig
 
 device = "cpu"
@@ -13,18 +39,20 @@ print(f"Using device: {device}")
 num_return_sequences = 10
 max_length = 100
 
-enc = tiktoken.get_encoding("gpt2")
-
-with open("text.txt", "r") as f:
-    text = f.read()
-text = text[:10000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buf = torch.tensor(tokens[:B*T + 1], device=device)
-x = buf[:-1].view(B, T)
-y = buf[1:].view(B, T)
+data = DataLoader(B=4, T=32)
 
 model = GPT(GPTConfig())
 model.to(device)
-logits = model(x)
-print(logits.shape)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for iter in range(50):
+    x, y = data.next_batch()
+    x, y = x.to(device), y.to(device)
+    optimizer.zero_grad()
+    logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {iter} loss: {loss.item()}")
+
+import sys; sys.exit()
+
